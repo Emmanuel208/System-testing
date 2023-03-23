0eb89b8cfe8ceea2bd3ea931060935207ce839e1
Emmanuel208 <emekaobasi121@gmail.com>
2023-03-23 21:40:01 +0000

    first commit

---
+++ testing-project/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mexii\Downloads\django-testing-tutorial-master\django-testing-tutorial-master\budgetproject\testing-project\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
